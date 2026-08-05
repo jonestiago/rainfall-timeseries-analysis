e7d1e3facfd1c1914adb7a9896e3bd4469dbeba0
Jones Silva <126691741+jonestiago@users.noreply.github.com>
2026-08-05 11:25:39 -0300

    Configurações iniciais

--- view/app.py
+++ view/app.py
@@ -7,4 +7,19 @@ import seaborn as sns
 import os
 
 # Configurações da página
-pass
+st.set_page_config(
+    page_title="Dashboard Pluviométrico - Tabatinga/AM (1982-2025)",
+    page_icon="🌧️",
+    layout="wide"
+)
+
+# Título do dashboard
+st.title("Análise Pluviométrica - Tabatinga/AM (1982-2025)")
+st.markdown(
+    '''
+    Este dashboard apresenta uma análise exploratória 
+    dos dados de precipitação da estação 469001,
+    localizada em Tabatinga - AM, no período de 1982 a 2025.
+    '''
+)
+
