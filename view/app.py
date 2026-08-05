40394325de24bc4c51c162ca49e0040f0199aaae
Jones Silva <126691741+jonestiago@users.noreply.github.com>
2026-08-05 10:24:06 -0300

    A03 - criação do diretório view (dashboard)

---
+++ view/app.py
@@ -0,0 +1,10 @@
+# Importando bibliotecas necessárias
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import os
+
+# Configurações da página
+pass
